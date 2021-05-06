18f6b16ed52b5ef576e6b0e9809f755c35d14fff
Joshua Barnett <joshua@barne.tt>
2021-05-06 17:43:20 +0100

    Refactor interceptor parts into class methods.

--- src/client.js
+++ src/client.js
@@ -17,155 +17,77 @@ const randomDelay = () =>
 
 const GOAWAY_RECEIVED_DETAILS = 'GOAWAY received'
 
-// https://github.com/grpc/grpc-node/blob/grpc%401.24.x/packages/grpc-native-core/test/client_interceptors_test.js#L464-L529
-// var maxRetries = 3
-// var retry_interceptor = function (options, nextCall) {
-//   var savedMetadata
-//   var savedSendMessage
-//   var savedReceiveMessage
-//   var savedMessageNext
-//   var requester = new RequesterBuilder()
-//     .withStart(function (metadata, listener, next) {
-//       savedMetadata = metadata
-//       var new_listener = new ListenerBuilder()
-//         .withOnReceiveMessage(function (message, next) {
-//           savedReceiveMessage = message
-//           savedMessageNext = next
-//         })
-//         .withOnReceiveStatus(function (status, next) {
-//           var retries = 0
-//           var retry = function (message, metadata) {
-//             retries++
-//             var newCall = nextCall(options)
-//             var receivedMessage
-//             newCall.start(metadata, {
-//               onReceiveMessage: function (message) {
-//                 receivedMessage = message
-//               },
-//               onReceiveStatus: function (status) {
-//                 if (status.code !== grpc.status.OK) {
-//                   if (retries <= maxRetries) {
-//                     retry(message, metadata)
-//                   } else {
-//                     savedMessageNext(receivedMessage)
-//                     next(status)
-//                   }
-//                 } else {
-//                   var new_status = new StatusBuilder()
-//                     .withCode(grpc.status.OK)
-//                     .build()
-//                   savedMessageNext(receivedMessage)
-//                   next(new_status)
-//                 }
-//               }
-//             })
-//             newCall.sendMessage(message)
-//             newCall.halfClose()
-//           }
-//           if (status.code !== grpc.status.OK) {
-//             // Change the message we're sending only for test purposes
-//             // so the server will respond without error
-//             var newMessage =
-//               savedMetadata.get('name')[0] === 'bar'
-//                 ? { value: 'bar' }
-//                 : savedSendMessage
-//             retry(newMessage, savedMetadata)
-//           } else {
-//             savedMessageNext(savedReceiveMessage)
-//             next(status)
-//           }
-//         })
-//         .build()
-//       next(metadata, new_listener)
-//     })
-//     .withSendMessage(function (message, next) {
-//       savedSendMessage = message
-//       next(message)
-//     })
-//     .build()
-//   return new InterceptingCall(nextCall(options), requester)
-// }
-
+// Based on: https://github.com/grpc/grpc-node/blob/grpc%401.24.x/packages/grpc-native-core/test/client_interceptors_test.js#L464-L529
+// attempted to mitigate some of the callback hell in that example.
 class GoawayRetryInterceptingCall extends grpc.InterceptingCall {
   constructor (options, nextCall) {
-    const { maxRetries = 3 } = options
-    let savedMetadata
-    let savedSendMessage
     super(nextCall(options), {
-      sendMessage: (message, next) => {
-        savedSendMessage = message
-        next(message)
-        return
+      sendMessage: (oldMessage, next) => {
+        const newMessage = this._sendMessage(oldMessage)
+        next(newMessage)
       },
       start: (metadata, _, next) => {
-        savedMetadata = metadata
-        return next(metadata, {
-          onReceiveStatus: (status, next) => {
-            const retry = (message, metadata, retries = 1) => {
-              metadata.set('retries', retries.toString())
-              const newCall = nextCall(options)
-              newCall.start(metadata, {
-                onReceiveStatus: status => {
-                  if (this.isRetryableStatus(status) && retries <= maxRetries) {
-                    retry(message, metadata, retries++)
-                    return
-                  }
-                  next(status)
-                  return
-                }
-              })
-              newCall.sendMessage(message)
-              newCall.halfClose()
-              return
-            }
-            if (this.isRetryableStatus(status)) {
-              retry(savedSendMessage, savedMetadata)
-              return
-            }
-            next(status)
-            return
-          }
-        })
+        const listener = this._start(metadata)
+        next(metadata, listener)
       }
     })
+    this._options = {
+      maxRetries: 3,
+      ...options
+    }
+    this._nextCall = nextCall
+    this._savedMetadata = null
+    this._savedSendMessage = null
   }
-  isRetryableStatus ({ code, details }) {
-    switch (true) {
-      case code === grpc.status.UNAVAILABLE &&
-        details === GOAWAY_RECEIVED_DETAILS:
-        return true
-      default:
-        return false
+  _sendMessage (message) {
+    this._savedSendMessage = message
+    return this._savedSendMessage
+  }
+  _start (metadata) {
+    this._savedMetadata = metadata
+    return {
+      onReceiveStatus: async (oldStatus, next) => {
+        try {
+          const newStatus = await this._onReceiveStatus(oldStatus)
+          next(newStatus)
+        } catch (error) {
+          next(error)
+        }
+      }
     }
   }
-}
-
-// Does nothing.
-class PassthroughInterceptingCall extends grpc.InterceptingCall {
-  constructor (options, nextCall) {
-    super(nextCall(options), {
-      start: function (metadata, listener, next) {
-        next(metadata, {
-          onReceiveMetadata: function (metadata, next) {
-            next(metadata)
-          },
-          onReceiveMessage: function (message, next) {
-            next(message)
-          },
-          onReceiveStatus: function (status, next) {
-            next(status)
+  _onReceiveStatus (status, retries = 0) {
+    if (this._isRetryable(status, retries)) {
+      retries++
+      return this._retry(this._savedSendMessage, this._savedMetadata, retries)
+    }
+    return status
+  }
+  _isRetryable ({ code, details }, retries) {
+    return (
+      retries <= this._options.maxRetries &&
+      code === grpc.status.UNAVAILABLE &&
+      details === GOAWAY_RECEIVED_DETAILS
+    )
+  }
+  _retry (message, metadata, retries) {
+    return new Promise((resolve, reject) => {
+      metadata.set('retries', retries.toString())
+      const newCall = this._nextCall(this._options)
+      newCall.start(metadata, {
+        onReceiveStatus: async (oldStatus, next) => {
+          try {
+            const newStatus = await this._onReceiveStatus(oldStatus, retries)
+            next(newStatus)
+            return resolve(newStatus)
+          } catch (error) {
+            next(error)
+            return reject(error)
           }
-        })
-      },
-      sendMessage: function (message, next) {
-        next(message)
-      },
-      halfClose: function (next) {
-        next()
-      },
-      cancel: function (message, next) {
-        next()
-      }
+        }
+      })
+      newCall.sendMessage(message)
+      newCall.halfClose()
     })
   }
 }
@@ -204,15 +126,6 @@ class Client {
       this.grpcClient.makeUnaryRequest(...args)
     )
   }
-  isRetryableError (error) {
-    switch (true) {
-      case error.code === grpc.status.UNAVAILABLE &&
-        error.details === GOAWAY_RECEIVED_DETAILS:
-        return true
-      default:
-        return false
-    }
-  }
   async exec (body = {}, retries = 0) {
     try {
       const { seq } = body
@@ -228,10 +141,6 @@ class Client {
     } catch (error) {
       const { seq } = body
       console.error(`${seq}: ${error.message}`)
-      // if (this.isRetryableError(error) && retries < this.retryLimit) {
-      //   return this.exec(body, retries++)
-      // }
-      throw error
     }
   }
   async run (repeat = REPEAT, delay = DELAY) {
